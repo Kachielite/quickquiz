317470565d3254fe1b070b52555269f325afe37f
Kachielite <40024356+Kachielite@users.noreply.github.com>
2020-04-09 20:24:55 +0100

    Update game.js

--- game.js
+++ game.js
@@ -193,7 +193,7 @@ function getNewQuestion() {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem("mostRecentScore", score);
         //go to the end page
-        return window.location.assign("/end.html");
+        return window.location.assign("../quickquiz/end.html");
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
